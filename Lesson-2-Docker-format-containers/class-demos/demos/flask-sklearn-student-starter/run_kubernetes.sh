ec77950ea723bc1924c3f91e814385138abd6ed5
Kavin Raju S <rajukavin25@gmail.com>
2020-09-15 13:57:54 +0000

    Added run_kubernetes.sh

--- Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/run_kubernetes.sh
+++ Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/run_kubernetes.sh
@@ -4,17 +4,21 @@
 
 # Step 1:
 # This is your Docker ID
-#dockerpath=""
+dockerpath=""
 
 # Step 2
 # Run in Docker Hub container with kubernetes
-
+kubectl run flaskskearlndemo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskskearlndemo
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to host
-
+kubectl port-forward flaskskearlndemo 8000:80
 
 
